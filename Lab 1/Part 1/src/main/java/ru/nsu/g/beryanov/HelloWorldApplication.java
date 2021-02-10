3614f662ed0b9957779b0d74b04f7b2331325b7a
Берьянов Максим Сергеевич <MSBeryanov@sberbank.ru>
2021-02-10 19:55:59 +0700

    Задача 1 -> Часть 1

---
+++ Lab 1/Part 1/src/main/java/ru/nsu/g/beryanov/HelloWorldApplication.java
@@ -0,0 +1,10 @@
+package ru.nsu.g.beryanov;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class HelloWorldApplication {
+    public static void main(String[] args) {
+        log.info("Hello World!");
+    }
+}
